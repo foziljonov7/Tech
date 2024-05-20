019190754ba7ec19ff1b9e1110fa2c65d16a0f32
foziljonov7 <abdulvosidfoziljonov55@gmail.com>
2024-05-20 16:20:01 +0500

    ...

---
+++ Tech.DAL/DTOs/SubjectDTOs/SubjectDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tech.DAL.DTOs.SubjectDTOs;
+
+public class SubjectDto
+{
+    [JsonPropertyName("id")]
+    public long Id { get; set; }
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+    [JsonPropertyName("category_id")]
+    public long CategoryId { get; set; }
+}
---
+++ Tech.Services/Services/Subjects/SubjectService.cs
@@ -0,0 +1,33 @@
+﻿using Tech.Domain.Entities;
+using Tech.Infrastructure.Interfaces;
+using Tech.Services.Interfaces.Generics;
+
+namespace Tech.Services.Services.Subjects;
+
+public class SubjectService<TEntity>(IRepository<Subject> repository) : IGettable<TEntity>, IModification<TEntity>
+{
+	public Task<TEntity> AddAsync(TEntity dto, CancellationToken cancellation = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<bool> RemoveAsync(long id, CancellationToken cancellation = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<IEnumerable<TEntity>> RetreiveAllAsync(CancellationToken cancellation = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<TEntity> RetreiveByIdAsync(long id, CancellationToken cancellation = default)
+	{
+		throw new NotImplementedException();
+	}
+
+	public Task<TEntity> UpdateAsync(long id, TEntity dto, CancellationToken cancellation = default)
+	{
+		throw new NotImplementedException();
+	}
+}
