e34bfa8635f67a6ac8231df5fc70f9198c0325a4
Tahsimuz <abdulvosidfoziljonov55@gmail.com>
2024-06-24 20:17:39 +0500

    ...

--- Tech.Web/Model/Account/RegisterModel.cs
+++ Tech.Web/Model/Account/RegisterModel.cs
@@ -1,10 +1,15 @@
-﻿namespace Tech.Web.Model.Account;
+﻿using System.Text.Json.Serialization;
+
+namespace Tech.Web.Model.Account;
 
 public class RegisterModel
 {
+    [JsonPropertyName("firstname")]
     public string Firstname { get; set; } = string.Empty;
+    [JsonPropertyName("lastname")]
     public string Lastname { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    [JsonPropertyName("phone_number")]
     public string PhoneNumber { get; set; } = string.Empty;
+    [JsonPropertyName("password")]
+    public string Password { get; set; } = string.Empty;
 }
