c3e93d2b2a2418d7bd752cbf9f3166406dba20a4
Tahsimuz <abdulvosidfoziljonov55@gmail.com>
2024-06-24 13:02:30 +0500

    Successfully completed Login page

--- Tech.Web/Model/Account/LoginModel.cs
+++ Tech.Web/Model/Account/LoginModel.cs
@@ -1,7 +1,11 @@
-﻿namespace Tech.Web.Model.Account;
+﻿using System.Text.Json.Serialization;
+
+namespace Tech.Web.Model.Account;
 
 public class LoginModel
 {
+    [JsonPropertyName("phone_number")]
     public string PhoneNumber { get; set; } = string.Empty;
+    [JsonPropertyName("password")]
     public string Password { get; set; } = string.Empty;
 }
